a9f36d01caa93aca47908d3a9053e39b5d531e78
Dakotah Ulrath <dulrath@live.com>
2022-08-21 22:29:40 -0400

    contact component, helper file, other files

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About () {
+    return (
+        <div>
+            <p className="">Currently a Student in a Coding/Fullstack Web Development Bootcamp</p>
+            <hr />
+            <img className="" src='/public/img/portfolio.jpg' alt='Dakotah James-Anthony Ulrath' />
+            <p>
+                I am Dakotah, i am currently a student in the Michigan State University Coding Bootcamp. I am studying to become a Fullstack Web Developer.
+            </p>
+            <p>
+                I am working toward the goal of becoming a Fullstack Web Developer. I want to build, design, and help create new websites. I want to be able to work with clients, and a team to make the best posstible websites and web services available. 
+            </p>
+        </div>
+    )
+}
+
+export default About;
