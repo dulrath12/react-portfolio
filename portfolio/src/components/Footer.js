aaaf4c679b16e31719025bbafd055f6c0997a01d
Dakotah Ulrath <dulrath@live.com>
2022-08-21 22:39:50 -0400

    header, footer, navigation compnents

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/dulrath12" target="_blank" rel="noreferrer">
+            Dakotah James-Anthony Ulrath
+          </a>
+          .
+          <hr />
+          <a
+            href="https://www.linkedin.com/in/dakotah-ulrath-2495a3235/"
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+          </a>{" "}
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
